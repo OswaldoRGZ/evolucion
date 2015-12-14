19bac2766901588434cc836db5ce194c85398d88
Oswaldo Pc Bestia <oswaldrgz@yahoo.es>
2015-12-14 11:29:45 -0500

    -Main externo

---
+++ src/controlador/Main.java
@@ -0,0 +1,20 @@
+package controlador;
+
+import java.awt.EventQueue;
+
+import vista.Menu;
+
+public class Main {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Menu frame = new Menu();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
